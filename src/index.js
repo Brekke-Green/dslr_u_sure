019935e3a4efcb7c49139ab1b0289006b829e6b2
Brekke Green <brekke.andrew.green@gmail.com>
2021-04-05 22:13:44 -0400

    Setting up for aperture drawing with canvas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles/index.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Webpack is working");
-    const canvasEl = document.getElementById("app-canvas");
-
+    const canvas = document.getElementById("app-canvas");
+    let ctx = canvas.getContext('2d');
+    
 });
