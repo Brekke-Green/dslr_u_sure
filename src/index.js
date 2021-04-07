f0c685383b902e74adb3714729ff7f44cc78ec5a
Brekke Green <brekke.andrew.green@gmail.com>
2021-04-06 20:12:14 -0400

    Connect aperture to slider

--- src/index.js
+++ src/index.js
@@ -5,54 +5,53 @@ document.addEventListener("DOMContentLoaded", () => {
     
     let apertureSlider = document.getElementById("Aperture");
     let apertureRadius = apertureSlider.value;
-    
-    // Update the current slider value (each time you drag the slider handle)
-    // slider.oninput = function() {
-    //   output.innerHTML = this.value;
-    // }
-    
-    let liveCanvas = document.getElementById("live-canvas");
-    let ctx = liveCanvas.getContext('2d');
-    // let photoCanvas = document.getElementById("photo-canvas");
-    // let ctx2 = photoCanvas.getContext('2d');
+
+    apertureSlider.oninput = function() {
+        apertureRadius = this.value;
+    };
 
     class Ball {
         constructor() {
             this.x = 100;
             this.y = 100;
             this.radius = apertureRadius;
-            this.color = 'blue';
+            this.color = 'black';
         }
 
-        drawLive() {
+        drawLive(ctx) {
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
             ctx.closePath();
             ctx.fillStyle = this.color;
             ctx.fill();
         }
-    };
+
+        update(ctx) {
+            this.radius = apertureRadius;
+            this.drawLive(ctx);
+        }
+    }
+
+    class ApertureCanvasDisplay {
+        constructor() {
+            this.canvas = document.getElementById("live-canvas");
+            this.ctx = this.canvas.getContext('2d');
+            this.width= 480;
+            this.height= 500;        
+            this.Ball = new Ball;
+            this.animate = this.animate.bind(this);
+        }
+        
+        animate() {
+            debugger
+            this.ctx.clearRect(0, 0, this.width, this.height);
+            this.Ball.update(this.ctx);
+            requestAnimationFrame(this.animate);
+        }
+    }
     
-    // let ball2 = {
-    //     x: 100,
-    //     y: 100,
-    //     radius: apertureRadius,
-    //     color: 'blue',
-    //     drawPhoto: () => {
-    //         ctx2.beginPath();
-    //         ctx2.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    //         ctx2.closePath();
-    //         ctx2.fillStyle = this.color;
-    //         ctx2.fill();
-    //     }
-    // };
-    
-
-    let ball = new Ball;
-    ball.drawLive();  
-    // ball2.drawPhoto();
-
-    requestAnimationFrame(ball.drawLive);
-
+    let apertureCanvasDisplay = new ApertureCanvasDisplay;
+    debugger
+    apertureCanvasDisplay.animate();
 });
 
