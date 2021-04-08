bef900c93f7e5b241049f92b170da22a5956e6ff
Brekke Green <brekke.andrew.green@gmail.com>
2021-04-07 22:57:00 -0400

    Create Live canvas to shoot with camera

--- src/index.js
+++ src/index.js
@@ -12,15 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     class Aperture {
         constructor() {
-            this.x = 100;
-            this.y = 300;
+            this.x = 150;
+            this.y = 250;
             this.radius = apertureRadius;
-            this.color = 'black';
+            this.color = "#24311B";
         }
 
         drawAperture(ctx) {
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.strokeStyle = "#A2A56E";
+            ctx.stroke();
             ctx.closePath();
             ctx.fillStyle = this.color;
             ctx.fill();
@@ -34,10 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     class Outline {
         constructor() {
-            this.x = 100;
-            this.y = 300;
+            this.x = 150;
+            this.y = 250;
             this.lensRadius = 70;
-            this.color = 'slategray';
+            this.color = '#0F1211';
         }
 
         drawLens(ctx) {
@@ -50,33 +52,66 @@ document.addEventListener("DOMContentLoaded", () => {
 
         drawBarrel(ctx) {
             ctx.beginPath();
-            ctx.arc(this.x, this.y, this.lensRadius+5, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fillStyle = 'black';
-            ctx.fill();
-
-            ctx.moveTo(this.x-50, this.y-50);
-            ctx.lineTo(this.x-50, this.y-200);
-            ctx.lineTo(this.x+250, this.y-200);
-            ctx.lineTo(this.x+250, this.y+50);
-            ctx.lineTo(this.x+50, this.y+50);
+            ctx.moveTo(this.x-80, this.y+10);
+            ctx.lineTo(this.x-80, this.y-180);
+            ctx.lineTo(this.x+205, this.y-170);
+            ctx.lineTo(this.x+205, this.y+30);
+            ctx.lineTo(this.x-80, this.y+10);
+            ctx.lineTo(this.x-80, this.y-180);
+            ctx.strokeStyle = "black";
             ctx.stroke();
-            ctx.fillStyle = "black";
+            ctx.closePath();
+            ctx.fillStyle = "#D6D6D4";
             ctx.fill();
 
+            ctx.beginPath();
+            ctx.moveTo(this.x-80, this.y-180);
+            ctx.lineTo(this.x-50, this.y-190);
+            ctx.lineTo(this.x+235, this.y-180);
+            ctx.lineTo(this.x+205, this.y-170);
+            ctx.lineTo(this.x-80, this.y-180);
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+            ctx.closePath();
+            ctx.fillStyle = "#ADABA7";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.moveTo(this.x+235, this.y-180);
+            ctx.lineTo(this.x+235, this.y+20);
+            ctx.lineTo(this.x+205, this.y+30);
+            ctx.lineTo(this.x+205, this.y-170);
+            ctx.lineTo(this.x+235, this.y-180);
+            ctx.lineTo(this.x+235, this.y+20);
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+            ctx.closePath();
+            ctx.fillStyle = "#908F90";
+            ctx.fill();
+
+            ctx.beginPath();
             ctx.moveTo(this.x-53, this.y-53);
             ctx.lineTo(this.x, this.y-106);
             ctx.lineTo(this.x+106, this.y);
             ctx.lineTo(this.x+53, this.y+53);
             ctx.lineTo(this.x-53, this.y-53);
+            ctx.strokeStyle = "#1F2021"
             ctx.stroke();
-            ctx.fillStyle = "black";
+            ctx.closePath();
+            ctx.fillStyle = "#1F2021";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.lensRadius+5, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = "#4A4A4A";
             ctx.fill();
 
             ctx.beginPath();
             ctx.arc(this.x+53, this.y-53, 75, (1/4) * Math.PI, (5/4)*Math.PI, true);
+            ctx.strokeStyle = "darkslategray";
             ctx.stroke();
-            ctx.fillStyle = "black";
+            ctx.fillStyle = "#1F2021";
             ctx.fill();
         }
 
@@ -98,7 +133,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         
         animate() {
-            debugger
             this.ctx.clearRect(0, 0, this.width, this.height);
             this.Outline.drawOutline(this.ctx);
             this.Aperture.update(this.ctx);
@@ -107,7 +141,96 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     let cameraCanvasDisplay = new CameraCanvasDisplay;
-    debugger
     cameraCanvasDisplay.animate();
+
+    class Plane  {
+        constructor() {
+            this.x = 240;
+            this.y = 320;
+            this.radius = 80;
+        }
+
+        drawFucilage(ctx) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = "red";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius-20, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = "darkslategray";
+            ctx.fill();
+        }
+
+        drawPropeller(ctx) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, 20, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = "yellow";
+            ctx.fill();
+
+            // ctx.beginPath();
+            // ctx.arc(this.x, this.y, this.radius-20, 0, Math.PI * 2, true);
+            // ctx.closePath();
+            // ctx.fillStyle = "darkslategray";
+            // ctx.fill();
+        }
+    }
+
+    class LiveCanvasDisplay {
+        constructor() {
+            this.canvas = document.getElementById("live-canvas");
+            this.ctx = this.canvas.getContext('2d');
+            this.width= 480;
+            this.height= 500;        
+            this.Plane = new Plane;
+            // this.animate = this.animate.bind(this);
+            // this.Outline = new Outline;
+        }
+
+        drawGrass(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(this.width, this.height);
+            ctx.lineTo(this.width, this.height-180);
+            ctx.lineTo(this.width-this.width, this.height-180);
+            ctx.lineTo(this.width-this.width, this.height);
+            ctx.lineTo(this.width, this.height);
+            ctx.strokeStyle = "darkgreen"
+            ctx.stroke();
+            ctx.closePath();
+            ctx.fillStyle = "green";
+            ctx.fill();
+        }
+
+        drawSky(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(this.width, this.height-180);
+            ctx.lineTo(this.width, this.height-this.height);
+            ctx.lineTo(this.width-this.width, this.height-this.height);
+            ctx.lineTo(this.width-this.width, this.height-180);
+            ctx.lineTo(this.width, this.height-180);
+            ctx.strokeStyle = "lightskyblue"
+            ctx.stroke();
+            ctx.closePath();
+            ctx.fillStyle = "deepskyblue";
+            ctx.fill();
+        }
+        
+        animate() {
+            this.ctx.clearRect(0, 0, this.width, this.height);
+            this.drawGrass(this.ctx);
+            this.drawSky(this.ctx);
+            // this.Outline.drawOutline(this.ctx);
+            this.Plane.drawFucilage(this.ctx);
+            this.Plane.drawPropeller(this.ctx);
+
+            // requestAnimationFrame(this.animate);
+        }
+    }
+
+    let liveCanvasDisplay = new LiveCanvasDisplay;
+    liveCanvasDisplay.animate();
 });
 
