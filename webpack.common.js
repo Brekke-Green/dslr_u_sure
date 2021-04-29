e5b82f4bc0e8f36c15d735836a65c6f2ecd25eee
Brekke Green <brekke.andrew.green@gmail.com>
2021-04-29 16:06:02 -0400

    Style camera display and update fonts

--- webpack.common.js
+++ webpack.common.js
@@ -58,6 +58,16 @@ module.exports = {
         ],
       },
       {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          },
+        },
+      },
+      {
         test: /\.s[ca]ss/i,
         use: [
           {
